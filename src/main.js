a2a1ddb94e24d3376b95b99673ecc3d0d69d8f8a
iC <244629604@qq.com>
2023-05-18 21:43:36 +0800

    layout静态结构搭建

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
-import './assets/main.css'
+// import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+
+import '@/styles/common.scss'
 
 const app = createApp(App)
 
