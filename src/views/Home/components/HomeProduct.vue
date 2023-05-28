6f6f79111cde38c6267a066b70b5e32674a4684e
iC <244629604@qq.com>
2023-05-28 22:58:17 +0800

    banner

---
+++ src/views/Home/components/HomeProduct.vue
@@ -0,0 +1,3 @@
+<template>
+  homeproduct
+</template>
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,3 +1,19 @@
+<script setup>
+import HomeBanner from './components/HomeBanner.vue'
+import HomeCategory from './components/HomeCategory.vue'
+import HomeNew from './components/HomeNew.vue'
+import HomeHot from './components/HomeHot.vue'
+import HomeProduct from './components/HomeProduct.vue'
+</script>
+
 <template>
-  home
+  <div class="container">
+    <HomeCategory />
+    <HomeBanner />
+  </div>
+  <HomeNew />
+  <HomeHot />
+  <HomeProduct />
 </template>
+
+<style lang="scss" scoped></style>
