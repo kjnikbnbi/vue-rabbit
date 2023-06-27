0a1b98a38fc0120526156d9cf5f445b4660c02f3
iC <244629604@qq.com>
2023-06-27 23:39:43 +0800

    本地购物车全选列表

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -3,6 +3,7 @@ import { computed, ref } from "vue"
 
 export const useCartStore = defineStore('cart', () => {
   const cartList = ref([])
+  // 添加购物车
   const addCart = (goods) => {
     const item = cartList.value.find((item) => goods.skuId === item.skuId)
     if (item) {
@@ -11,17 +12,36 @@ export const useCartStore = defineStore('cart', () => {
       cartList.value.push(goods)
     }
   }
+  // 删除购物车
   const delCart = (skuId) => {
     const idx = cartList.value.findIndex((item) => skuId === item.skuId)
     cartList.value.splice(idx, 1)
   }
+  // 单选功能
+  const singleCheck = (skuId, selected) => {
+    const item = cartList.value.find((item) => item.skuId === skuId)
+    item.selected = selected
+  }
+  // 全选
+  const allCheck = (selected) => {
+    cartList.value.forEach(item => item.selected = selected)
+  }
+  // 是否全选
+  const isAll = computed(() => cartList.value.every((item) => item.selected))
+  // 总价
   const allCount = computed(() => cartList.value.reduce((a, b) => a + b.count, 0))
   const allPrice = computed(() => cartList.value.reduce((a, b) => a + b.count * b.price, 0))
   return {
     cartList,
     addCart,
     delCart,
+    allCheck,
+    isAll,
     allCount,
-    allPrice
+    allPrice,
+    singleCheck
   }
-})
+},
+  {
+    persist: true,
+  })
