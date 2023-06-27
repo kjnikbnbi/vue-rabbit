f4e7bf42552ab57a9efdb55f4d49a1117eaf61a2
iC <244629604@qq.com>
2023-06-27 18:58:34 +0800

    购物车删除商品

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -11,24 +11,13 @@ export const useCartStore = defineStore('cart', () => {
       cartList.value.push(goods)
     }
   }
+  const delCart = (skuId) => {
+    const idx = cartList.value.findIndex((item) => skuId === item.skuId)
+    cartList.value.splice(idx, 1)
+  }
   return {
     cartList,
-    addCart
+    addCart,
+    delCart
   }
-  // const addCart = (goods) => {
-  //   const { skuId, count } = goods
-  //     // 添加购物车操作
-  //     // 已添加过 - count + 1
-  //     // 没有添加过 - 直接push
-  //     // 思路：通过匹配传递过来的商品对象中的skuId能不能在cartList中找到，找到了就是添加过
-  //     const item = cartList.value.find((item) => goods.skuId === item.skuId)
-  //     if (item) {
-  //       // 找到了
-  //       item.count++
-  //     } else {
-  //       // 没找到
-  //       cartList.value.push(goods)
-  //     }
-  //   }
- 
 })
