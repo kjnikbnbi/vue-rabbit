26d3da78d7c053bb119fc136477edd9dc1296a77
iC <244629604@qq.com>
2023-06-30 10:53:04 +0800

    结算模块 基础渲染

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -1,21 +1,46 @@
 import { defineStore } from "pinia"
 import { computed, ref } from "vue"
+import { useUserStore } from "./user"
+import { insertCartAPI, delCartAPI, findNewCartListAPI } from "@/apis/cart"
 
 export const useCartStore = defineStore('cart', () => {
+  const userStore = useUserStore()
+  const isLogin = computed(() => userStore.userInfo.token)
   const cartList = ref([])
+  // 获取最新购物车列表
+  const updataNewList = async () => {
+    const res = await findNewCartListAPI()
+    cartList.value = res.result
+  }
   // 添加购物车
-  const addCart = (goods) => {
-    const item = cartList.value.find((item) => goods.skuId === item.skuId)
-    if (item) {
-      item.count++
+  const addCart = async (goods) => {
+    const { skuId, count } = goods
+    if (isLogin.value) {
+      // 登陆之后加入购物车
+      await insertCartAPI({ skuId, count })
+      updataNewList()
     } else {
-      cartList.value.push(goods)
+      const item = cartList.value.find((item) => goods.skuId === item.skuId)
+      if (item) {
+        item.count++
+      } else {
+        cartList.value.push(goods)
+      }
     }
   }
   // 删除购物车
-  const delCart = (skuId) => {
-    const idx = cartList.value.findIndex((item) => skuId === item.skuId)
-    cartList.value.splice(idx, 1)
+  const delCart = async (skuId) => {
+    if (isLogin.value) {
+      await delCartAPI([skuId])
+      updataNewList()
+    } else {
+      const idx = cartList.value.findIndex((item) => skuId === item.skuId)
+      cartList.value.splice(idx, 1)
+    }
+  }
+  // 清除购物车
+  const clearCart = () => {
+    cartList.value = []
   }
   // 单选功能
   const singleCheck = (skuId, selected) => {
@@ -36,6 +61,7 @@ export const useCartStore = defineStore('cart', () => {
   return {
     cartList,
     addCart,
+    clearCart,
     delCart,
     allCheck,
     isAll,
@@ -43,7 +69,8 @@ export const useCartStore = defineStore('cart', () => {
     allPrice,
     selectedCount,
     selectedPrice,
-    singleCheck
+    singleCheck,
+    updataNewList
   }
 },
   {
--- src/stores/user.js
+++ src/stores/user.js
@@ -1,15 +1,30 @@
 import { loginAPI } from "@/apis/user"
 import { defineStore } from "pinia"
 import { ref } from "vue"
+import { useCartStore } from "./cartStore"
+import { mergeCartAPI } from "@/apis/cart"
 
 export const useUserStore = defineStore('user', () => {
+  const cartStore = useCartStore()
   const userInfo = ref({})
   const getUserInfo = async ({ account, password }) => {
     const res = await loginAPI({ account, password })
     userInfo.value = res.result
+    // 合并购物车
+    await mergeCartAPI(cartStore.cartList.map(item => {
+      return {
+        skuId: item.skuId,
+        selected: item.selected,
+        count: item.count
+      }
+    }))
+    cartStore.updataNewList()
   }
+
+  // 退出登录后清除用户信息
   const clearUserInfo = () => {
     userInfo.value = {}
+    cartStore.clearCart()
   }
   return {
     userInfo,
